a2a8185e42013cfa8f4b823abb5c45287bc273ab
Julian David <jdavid142@misena.edu.co>
2023-03-12 18:48:48 -0500

    Frutas interfaz

--- GameJump_3D/Assets/Code/Frutas_Contador.cs
+++ GameJump_3D/Assets/Code/Frutas_Contador.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Frutas_Contador : MonoBehaviour
 {
-    private int Score_M;
-    private int Score_C;
-    private int Score_W;
+    [SerializeField] private int Score_M;  
+    [SerializeField] private int Score_C;
+    [SerializeField] private int Score_W;
+    [SerializeField] private int Money;
+
     public TextMeshProUGUI Manzanas;
     public TextMeshProUGUI Melon;
     public TextMeshProUGUI Cereza;
+    public TextMeshProUGUI Money_;
+
+    public GameObject Manzana_image;
+    public GameObject Cherry_image;
+    public GameObject Melon_image;
+
     void Start()
     {
         Score_M = 0;
@@ -20,17 +29,59 @@ public class Frutas_Contador : MonoBehaviour
         if (collision.gameObject.tag == "Manzana")
         {
             Score_M++;
-            Manzanas.text = "Manzanas = " + "" + Score_M;
-        }
+            Manzanas.text = "" + Score_M;
+            Manzana_image.SetActive(true);
+            
+        } 
         if (collision.gameObject.tag == "Melon")
         {
             Score_C++;
-            Melon.text = "Melon = " + "" + Score_C;
+            Melon.text = "" + Score_C;        
+            Melon_image.SetActive(true);
         }
         if (collision.gameObject.tag == "Cereza")
         {
             Score_W++;
-            Cereza.text = "Cereza = " + "" + Score_W;
+            Cereza.text = "" + Score_W;
+            Cherry_image.SetActive(true);
+        }
+
+        // Dinero 
+        if(collision.gameObject.tag == "Money")
+        {
+            if(Score_M > 0) 
+            {
+               Score_M--;
+               Manzanas.text = "" + Score_M;
+
+                if(Score_M == 0)
+                {
+                    Money += 10;
+                    Money_.text = "Dinero = " + "" + Money;
+                }
+            }
+            if (Score_C > 0)
+            {
+               Score_C--;
+               Cereza.text = "" + Score_C;
+
+                if (Score_C >= 0)
+                {
+                    Money += 10;
+                    Money_.text = "Dinero = " + "" + Money;
+                }
+            }
+            if (Score_W > 0)
+            {
+                Score_W--;
+                Melon.text = "" + Score_W;
+
+                if (Score_W >= 0)
+                {
+                    Money += 10;
+                    Money_.text = "Dinero = " + "" + Money;
+                }
+            } 
         }
     }
 }
